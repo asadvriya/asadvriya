7dd2f202b3e80c622b60e50676bcf08d06c831f2
asadvriya <asadvriya@gmail.com>
2023-05-28 17:17:22 +0600

    Header Added

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './header.css';
+import logo from '../../../assets/name-logo.png';
 
 export default function Header() {
   return (
     <header className="header">
-        Header
+        <div className="name-logo">
+          <img src={logo} alt="" />          
+        </div>
+        <h1 className='site-title'>
+          <span>AvR</span>
+          <span> Md. Asad Chowdhary</span>
+        </h1>
     </header>
   );
 };
