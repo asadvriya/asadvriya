79733569fb731952a784bfe999c8c2fccd09273a
asadvriya <asadvriya@gmail.com>
2023-05-31 02:35:49 +0600

    Adding First Content.

--- src/components/pages/homepage/homepage.jsx
+++ src/components/pages/homepage/homepage.jsx
@@ -3,13 +3,55 @@ import React from 'react'
 export default function HomePage() {
   return (
     <main>
-      <main>
-        Homepage
-      </main>
-      Homepage
-      <main>
-        Homepage
-      </main>
+      <article>
+        <pre  >
+          <header>
+            <h1> Begins with Your Name</h1>
+          </header>
+          <p>
+            <section>
+              Your name is like a song, <br />
+              So sweet and full of grace, <br />
+              It flows like water through my soul, <br />
+              And fills me with embrace.
+            </section>
+
+            <section>
+              Your eyes are like the stars, <br />
+              So bright and full of light, <br />
+              They guide me through the darkness, <br />
+              And keep me safe at night.
+            </section>
+
+            <section>
+              Your smile is like the sun, <br />
+              So warm and full of love,<br />
+              It melts away my fears,<br />
+              And makes me feel above.
+            </section>
+
+            <section>
+              Your touch is like a dream,<br />
+              So soft and full of peace,<br />
+              It takes me to a place of magic,<br />
+              Where nothing can be seen.
+            </section>
+
+            <section>
+              Riya, you are my everything,<br />
+              My light, my love, my star,<br />
+              I will love you forever,<br />
+              And never let you go.
+            </section>
+
+          </p>
+          <footer>
+            <address>... <span className="author">Md. Asad Chowdhary</span> <time>31 May, 2023</time>  </address>
+          </footer>
+
+        </pre>
+
+      </article>
     </main>
 
   );
