458019008947f0e2f5b6e36e5103f13bcde1f921
asadvriya <asadvriya@gmail.com>
2023-07-14 15:05:27 +0600

    Modified some changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Suspense, lazy } from 'react';
+import About from './components/pages/about/about';
+import AboutMe from './components/pages/aboutMe/aboutMe';
 const HomePage = lazy(() => import("./components/pages/homepage/homepage"));
 const Layout = lazy(() => import("./components/layout/layout"));
 
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Suspense fallback={<div>loading...</div>}><Layout /></Suspense>}>
           <Route index element={<Suspense fallback={<div>loading...</div>}><HomePage /></Suspense>} />
+          <Route path="/about" elemetn={<Suspense fallback={<div>loading...</div>}><About /></Suspense>}/>
+          <Route path="/aboutMe" elemetn={<Suspense fallback={<div>loading...</div>}><AboutMe /></Suspense>}/>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/pages/about/about.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './about.css'
+
+export default function About() {
+  return (
+    <div>This is a personal blog of mine. I intentionally create it for my personal bio. I would like to share my thoughts, ideas and literrary as I like to enjoy writing. So, here I'll write some of my writings too. I am thinking that you would like them.</div>
+  )
+}
